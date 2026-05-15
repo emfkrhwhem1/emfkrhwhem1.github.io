dc49b6e3d16d9e17813be7b7817290549e98bc4c
emfkrhwhem1 <emfkrhwhem1@naver.com>
2026-05-15 15:10:32 +0900

    change the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"; // 추가
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/emfkrhwhem1.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // '@' 를 'src' 폴더로 매핑
